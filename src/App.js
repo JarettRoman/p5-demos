f04f55e8a85492631896a9bba34258d475496922
JarettRoman <afrocious@gmail.com>
2024-01-20 11:49:35 -0500

    fixing routes after getting rid of old site

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
                     <Router>
                         <Routes>
                             <Route path="/" element={<Layout />}>
-                                <Route path="p5-demos" element={<Home />} />
+                                <Route index path="p5-demos" element={<Home />} />
                                 <Route
                                     path="blackjack"
                                     element={<Blackjack />}
